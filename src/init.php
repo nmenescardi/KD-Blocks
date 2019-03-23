12bd3260b3804a091590ffb6e1148334902ee75d
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-03-23 10:29:37 -0400

    Add Bootstrap Grid System

--- src/init.php
+++ src/init.php
@@ -28,6 +28,13 @@ function kd_blocks_cgb_block_assets() { // phpcs:ignore
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
 	);
 
+	wp_enqueue_style(
+		'kd_blocks-bootstrap-grid-system', // Handle.
+		plugins_url( 'dist/lib/bootstrap-grid.min.css', dirname( __FILE__ ) ), // Block style CSS.
+		array( 'wp-editor' ) // Dependency to include the CSS after it.
+		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
+	);
+
 	wp_enqueue_script(
 		'kd_blocks-animations-accordion-js', // Handle.
 		plugins_url( '/dist/animations/accordion.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
